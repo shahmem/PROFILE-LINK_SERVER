3e2b976e1061b96789fc4935d38ca41c72a1b072
shahmem <kpmshameemudheenkp@gmail.com>
2025-09-26 11:43:13 +0530

    Save local changes

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
-const connectDB = require('./config/db');
 connectDB();
 
 const UserRoutes = require("./routes/user");
 const LinkRoutes = require("./routes/links");
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, 
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/profiles", UserRoutes);
-app.use("/api", LinkRoutes);
+app.use("/api/links", LinkRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
- 
